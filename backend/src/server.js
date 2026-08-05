3d4920742df0c9ea74155b359fffcd3e9305a4a4
Ayushsaxena0227 <saxenaayush381@gmail.com>
2026-08-05 16:29:58 +0530

    fix: place entry file inside src

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const connectDB = require("./src/config/db");
-const studentRoutes = require("./src/routes/student.routes");
+const connectDB = require("./config/db");
+const studentRoutes = require("./routes/student.routes");
 
 dotenv.config();
 
